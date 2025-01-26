e6a4aa0d8fa1a01a7c34870cc7ad0e78d13b1302
sourabh-garg <sourabhgrg713@gmail.com>
2025-01-26 15:10:41 +0530

    refactor: convert to standalone package
    - Remove Lerna monorepo setup
    - Move core package to root
    - Update package.json and dependencies
    - Configure TypeScript and Jest
    - Update documentation and configuration files

--- jest.config.js
+++ jest.config.js
@@ -1,62 +1,74 @@
 /* eslint-disable no-undef */
 module.exports = {
-    rootDir: __dirname,
-    roots: ['<rootDir>'],
-    reporters: ['default', 'jest-junit'],
-    coverageDirectory: 'target',
-    cache: true,
-    verbose: true,
-    cacheDirectory: '<rootDir>/tmp/jest',
-    moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
-    testMatch: ['**/?(*.)+(test|spec).(js|jsx|ts|tsx)'],
-    testPathIgnorePatterns: [
-        '/node_modules/',
-        '/tmp/',
-        '/coverage/',
-        '/stories/',
-        '/\\.storybook/',
-        '/scripts/',
-        '/dist/',
-        '/.cache/',
-        '.*\\.visual\\.(spec|test)\\.js',
+  preset: 'ts-jest',
+  rootDir: __dirname,
+  roots: ['<rootDir>'],
+  testEnvironment: 'jsdom',
+  setupFiles: ['./jest.polyfills.js'],
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: 'target',
+      outputName: 'junit.xml',
+      suiteName: 'vast-core'
+    }]
+  ],
+  coverageDirectory: 'target',
+  cache: true,
+  verbose: true,
+  cacheDirectory: '<rootDir>/tmp/jest',
+  moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
+  testMatch: ['**/?(*.)+(test|spec).(js|jsx|ts|tsx)'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/tmp/',
+    '/coverage/',
+    '/stories/',
+    '/\\.storybook/',
+    '/scripts/',
+    '/dist/',
+    '/.cache/',
+    '.*\\.visual\\.(spec|test)\\.js',
+  ],
+  watchPathIgnorePatterns: ['/node_modules/'],
+  automock: false,
+  unmockedModulePathPatterns: ['/node_modules/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts'
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '\\.json$', '/tests/', '/stories/', '/\\.storybook/'],
+  transformIgnorePatterns: ['/node_modules/(?!(lodash-es|[^/]+/es|rc-animate|rc-util)/).*'],
+  transform: {
+    '\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: `${__dirname}/tsconfig.json`,
+        diagnostics: false,
+        isolatedModules: true,
+      },
     ],
-    watchPathIgnorePatterns: ['/node_modules/'],
-    automock: false,
-    unmockedModulePathPatterns: ['/node_modules/'],
-    collectCoverage: false,
-    collectCoverageFrom: [],
-    coveragePathIgnorePatterns: ['/node_modules/', '\\.json$', '/tests/', '/stories/', '/\\.storybook/'],
-    transformIgnorePatterns: ['/node_modules/(?!(lodash-es|antd|[^/]+/es|rc-animate|rc-util)/).*'],
-    transform: {
-        '\\.(ts|tsx)$': [
-            'ts-jest',
-            {
-                tsconfig: `${__dirname}/tsconfig.json`,
-                diagnostics: false,
-                isolatedModules: true,
-            },
-        ],
-        '/node_modules/((lodash-es|[^/]+/es)|rc-animate|rc-util)/.*': [
-            'ts-jest',
-            {
-                tsconfig: `${__dirname}/tsconfig.json`,
-                diagnostics: false,
-                isolatedModules: true,
-            },
-        ],
-    },
-    preset: 'ts-jest',
-    globals: {
-        'ts-jest': {
-            tsConfig: `${__dirname}/tsconfig.json`,
+    '/node_modules/((lodash-es|[^/]+/es)|rc-animate|rc-util)/.*': [
+      'ts-jest',
+      {
+        tsconfig: `${__dirname}/tsconfig.json`,
+        diagnostics: false,
+        isolatedModules: true,
+      },
+    ],
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: `${__dirname}/tsconfig.json`,
 
-            // https://huafu.github.io/ts-jest/user/config/diagnostics
-            diagnostics: false,
+      // https://huafu.github.io/ts-jest/user/config/diagnostics
+      diagnostics: false,
 
-            // Makes jest test run much faster, BUT, without type checking.
-            // Type checking in CI is done with `tsc --noEmit` or `yarn typecheck` command.
-            // https://huafu.github.io/ts-jest/user/config/isolatedModules
-            isolatedModules: true,
-        },
+      // Makes jest test run much faster, BUT, without type checking.
+      // Type checking in CI is done with `tsc --noEmit` or `yarn typecheck` command.
+      // https://huafu.github.io/ts-jest/user/config/isolatedModules
+      isolatedModules: true,
     },
+  },
 };
